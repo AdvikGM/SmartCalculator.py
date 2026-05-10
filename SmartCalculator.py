5236a47b0e46b50ec99575d1938ddd82df0fa2a6
Advik G M <vaishnavi.dec04@gmail.com>
2026-05-10 10:32:33 +0530

    SmartCalculator.py

--- SmartCalculator.py
+++ SmartCalculator.py
@@ -1,25 +1,31 @@
-print("Smart Calculator")
-print("1.Additon")
-print("2.Subtraction")
-print("3.Multiplication")
-print("4.Division")
+while True:
+    print("Smart Calculator")
+    print("1.Additon")
+    print("2.Subtraction")
+    print("3.Multiplication")
+    print("4.Division")
 
-choice = input("Choose your operation:")
+    choice = input("Choose your operation:")
 
-num1 = float(input("Enter the first number:"))
-num2 = float(input("Enter the second number:"))
+    num1 = float(input("Enter the first number:"))
+    num2 = float(input("Enter the second number:"))
 
-if choice == "1":
-    print(num1 + num2)
+    if choice == "1":
+        print(num1 + num2)
 
-elif choice == "2":
-    print(num1 - num2)
+    elif choice == "2":
+        print(num1 - num2)
 
-elif choice == "3":
-    print(num1 * num2)
+    elif choice == "3":
+        print(num1 * num2)
 
-elif choice == "4":
-    print(num1 / num2)
+    elif choice == "4":
+        print(num1 / num2)
 
+    else:
+        print("Invalid choice")
 
-    
+    again = input("Do you want to continue? (y/n): ")
+
+    if again == "n":
+        break
